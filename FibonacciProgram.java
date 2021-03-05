0b5c42708f2777cf7cebf65bf8d5120b5f33ba32
Neilcarlo217 <80097175+Neilcarlo217@users.noreply.github.com>
2021-03-05 19:43:00 +0530

    Update FibonacciProgram.java

--- FibonacciProgram.java
+++ FibonacciProgram.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class test1{
-	public static int fib(int n){
+	public static int fib(int n){#function
 	if(n == 0)
 	{
 		return 0;
@@ -12,16 +12,16 @@ public class test1{
     }
 	else
 	{
-		return fib(n-2) + fib(n-1);
+		return fib(n-2) + fib(n-1);#recursive function
     }
 }
     public static void main(String args[]) {
-	Scanner scan = new Scanner(System.in);
-        System.out.print("Enter how many fibonacci no's are needed: ");
+	Scanner scan = new Scanner(System.in); #to get the no from user
+        System.out.print("Enter how many fibonacci no's are needed: ");#gives how many numbers need to be printed
         int num = scan.nextInt();
-	    System.out.print("Fibonacci Series of "+num+" numbers: ");
+	    System.out.print("Fibonacci Series of "+num+" numbers: ");# to print the series of numbers
 	    for(int i = 0; i < num; i++){
-			System.out.print(fib(i) +" ");
+			System.out.print(fib(i) +" ");#calls the function
 		}
 	}
 }
